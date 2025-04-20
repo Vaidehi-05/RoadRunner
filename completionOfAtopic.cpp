bef335f869340dde1253157bae5bfefb7c6b6cd7
Vaidehi-05 <siasrivastava14@gmail.com>
2025-04-20 22:09:45 +0530

    Create completionOfAtopic.cpp
    Storing just the traversal of the current graph levelwise, not storing any sub-traversals as that'll divert the custom flow.

---
+++ completionOfAtopic.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// CODE FOR COMPLETING A TOPIC FROM SCRATCH TO ADVANCED
+
+static bool comp_for_temp_pq(pair <string, int> a, pair <string, int> b)
+{
+    return a.second>b.second;
+}
+
+vector<string> completionOfATopic(string topicName)
+{
+    Graph topic=uni_map[topicName];
+    map <int, bool> vis;
+    stack <string> st;
+    vector <string> completePath;
+    priority_queue <pair<string, int>, vector<string, int>, comp_for_temp_pq> pq;
+    st.push(topicName);
+
+    while(!st.empty())
+    {
+        int curr=node_name[topic.st.top()];
+        if(!vis[topic.node_name[st.top()]])
+        completePath.push_back(st.top());  
+        vis[topic.node_name[st.top()]]=true;
+        st.pop();
+        
+        for(int i=0;i<topic.no_of_nodes;i++)
+        {
+            if(adjMat[curr][i].first!=-1&&vis[topic.node_index[i]]==false)  //TO-DO: CREATE A MAP mp<int, string> node_index; in the graph class that stores name of every index, opp. of what node_name is doing.
+            pq.push_back({topic.node_index[i], topic.adjMat[curr][i].first});
+        }
+
+        while(!pq.empty())  //STORED TEMPORARILY IN THE PRIORITY QUEUE SO THAT NODES ARE ENTERED IN THE STACK ACC. TO THE ORDER
+        {
+            st.push_back(pq.top());
+            pq.pop();
+        }
+    }
+    return completePath;
+}
