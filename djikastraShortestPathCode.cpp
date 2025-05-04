6171d871c830756c0cfe2e5bb25ebc9bfee6b786
Vaidehi-05 <siasrivastava14@gmail.com>
2025-05-04 17:36:40 +0530

    Update djikastraShortestPathCode.cpp
    review done --sia

--- djikastraShortestPathCode.cpp
+++ djikastraShortestPathCode.cpp
@@ -8,7 +8,7 @@ static bool comp(pair <Graph, int> a, pair <Graph, int> b)
 }
 
 
-vector <string> findMostOptimalPath(string start, int start_lvl, string end, int end_lvl, int alpha_val, int beta_val)
+vector <string> findMostOptimalPath(string start, int start_lvl, string end, int end_lvl, int alpha_val, int beta_val, map<string, int> presnt_skillset)
 {
     map <string, int> vis,shortest; //map storing shortet distance of all uptil now visited
     priority_queue <pair<Graph, int>, vector<pair<Graph,int>>, comp> pq;  //pq storing all nodes with their minimum distances
@@ -17,8 +17,8 @@ vector <string> findMostOptimalPath(string start, int start_lvl, string end, int
     Graph start_node, end_node;
 
     vis[start]=1;
-    start_node=unimap[start];
-    end_node=unimap[end];
+    start_node=uni_map[start];
+    end_node=uni_map[end];
     shortest[start]=0;
     shortest_path.push_back(start);
     pq.push({start_node, 0});
@@ -29,26 +29,30 @@ vector <string> findMostOptimalPath(string start, int start_lvl, string end, int
             int minDist=pq.top().second, curr=0;
             string st;
             pq.pop();
-        
+
             st=s.name_of_graph;
+            if(!vis[st])
+            shortest_path.push_back(st);
             curr=s.node_ind[st];
-            vis[st]=1
+            vis[st]=1;
 
             vector<vector<pair<int, int>>> adjMat=s.adjMat;
 
             for(int i=0; i<s.no_of_nodes; i++)  //traversing all the connections of the current node
             {
-                if(adjMat[curr][i].first!=0&&vis.find(s.node_name[i])==mp.end()))
+                if(adjMat[curr][i].first!=0&&vis.find(s.node_name[i])==vis.end())
                 {
                     int x=(alpha*adjMat[curr][i].first)+(beta*adjMat[curr][i].second);  //custom comparator 
-                    if(present_skillset[uni_map[s.node_name[i]]==present_skillset.end())  //create a present_skillset map to store current skillset of user
-                    pq.push_back({uni_map[s.node_name[i]], x});  
-                    shortest[s.node_name[i]]=minDist+x;
-                    shortest_path.push_back(s.node_name[i];
+                    if(present_skillset.find(s.node_name[i])==present_skillset.end())  //create a present_skillset map to store current skillset of user
+                    {
+                        pq.push({uni_map[s.node_name[i]], x});  
+                        shortest[s.node_name[i]]=minDist+x;
+                    }
 
                     if(s.node_name[i]==end)  //returning the path as soon as the ending node is added 
                     return shortest_path;
                 }
             }
+        return shortest_path;
     }
 }
